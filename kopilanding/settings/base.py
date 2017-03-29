320cad9d661db211ec8df18a391feb11b371f122
tagil <parser.p1@yandex.ru>
2017-03-29 11:32:30 +0300

    fix default DATABASES in settings.base.py

--- kopilanding/settings/base.py
+++ kopilanding/settings/base.py
@@ -66,12 +66,8 @@ WSGI_APPLICATION = 'kopilanding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '...',
-        'USER': '...',
-        'PASSWORD': '...',
-        # 'HOST': '',
-        # 'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
