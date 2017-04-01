c66fad3a5fbc1ca83ca2122179299dae71163a2e
kopi-dev <kopidev@yandex.ru>
2017-04-01 16:08:26 +0300

    create phone number to Page model. Create order model

--- landing/models.py
+++ landing/models.py
@@ -6,6 +6,7 @@ class Page(models.Model):
 
     project_name      = models.CharField('Название проекта', max_length=40)
     title             = models.CharField('Заголовок', max_length=200)
+    phone             = models.CharField('Телефон', max_length=20)
     title_description = models.TextField('Текст под заголовком', blank=True, null=True)
     title_btn         = models.CharField('Текст кнопки', max_length=50, default='Learn')
     title_btn_url     = models.CharField('URL кнопки', max_length=50, blank=True, null=True)
